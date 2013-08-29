6af81b7d582f29c70f6476d53390658dfa3efd99
Shaozhen Ding <dsz0111@gmail.com>
2013-08-28 21:41:51 -0500

    Fix is full method with backwash

--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -1,105 +1,107 @@
 public class Percolation {
-	private WeightedQuickUnionUF wQuickUnion;
-	private int width;
-	// false means block, while true means open
-	private boolean[] site;
-	private int numOfObjects;
-	int topRootNode;
-	int botomRootNode;
+    private WeightedQuickUnionUF wQuickUnion;
+    private int width;
+    // false means block, while true means open
+    private boolean[] site;
+    private int numOfObjects;
+    private int topRootNode;
+    private int botomRootNode;
 
-	public Percolation(int N) {
-		numOfObjects = N * N;
-		site = new boolean[numOfObjects];
-		width = N;
-		wQuickUnion = new WeightedQuickUnionUF(N * N + 2);
-		for (int i = 0; i < numOfObjects; i++) {
-			site[i] = false;
-		}
-		topRootNode = N*N;
-		botomRootNode = N*N + 1;
-	}
+    public Percolation(int N) {
+        numOfObjects = N * N;
+        site = new boolean[numOfObjects];
+        width = N;
+        wQuickUnion = new WeightedQuickUnionUF(N * N + 2);
+        for (int i = 0; i < numOfObjects; i++) {
+            site[i] = false;
+        }
+        topRootNode = N * N;
+        botomRootNode = N * N + 1;
+    }
 
-	public void open(int i, int j) {
-		if (!valid(i, j)) {
-			throw new IndexOutOfBoundsException("Site: [" + i + "][" + j + "]");
-		}
-		int position = index(i, j);
-		site[position] = true;
-		//virtulization of the root node
-		if( i == 1){
-			wQuickUnion.union(position, topRootNode);
-		}
-		if( i == width){
-			wQuickUnion.union(position, botomRootNode);
-		}
-		int[] neighbors = getNeighbors(i, j);
-		for (int n = 0; n < neighbors.length; n++) {
-			if (site[neighbors[n]]) {
-				wQuickUnion.union(position, neighbors[n]);
-			}
-		}
-	}
+    public void open(int i, int j) {
+        if (!valid(i, j)) {
+            throw new IndexOutOfBoundsException("Site: [" + i + "][" + j + "]");
+        }
+        int position = index(i, j);
+        site[position] = true;
+        // virtulization of the root node
+        if (i == 1) {
+            wQuickUnion.union(topRootNode, position);
+        }
+        if (i == width && !percolates()) {
+            wQuickUnion.union(position, botomRootNode);
+        }
+        int[] neighbors = getNeighbors(i, j);
+        for (int n = 0; n < neighbors.length; n++) {
+            if (site[neighbors[n]]) {
+                wQuickUnion.union(position, neighbors[n]);
+            }
+        }
+    }
 
-	public boolean isOpen(int i, int j) {
-		return site[index(i, j)];
-	}
+    public boolean isOpen(int i, int j) {
+        if (!valid(i, j)) {
+            throw new IndexOutOfBoundsException("Site: [" + i + "][" + j + "]");
+        }
+        return site[index(i, j)];
+    }
 
-	public boolean isFull(int i, int j) {
-		if(!valid(i,j)){
-			throw new IndexOutOfBoundsException("Site: [" + i + "][" + j + "]");
-		}
-		int position = index(i, j);
-		if (!site[position]) {
-			return false;
-		}
+    public boolean isFull(int i, int j) {
+        if (!valid(i, j)) {
+            throw new IndexOutOfBoundsException("Site: [" + i + "][" + j + "]");
+        }
+        int position = index(i, j);
+        if (!site[position]) {
+            return false;
+        }
 
-		return wQuickUnion.connected(position, topRootNode);
-	}
+        return wQuickUnion.connected(position, topRootNode);
+    }
 
-	public boolean percolates() {
-		return wQuickUnion.connected(topRootNode, botomRootNode);
-	}
+    public boolean percolates() {
+        return wQuickUnion.connected(topRootNode, botomRootNode);
+    }
 
-	protected int index(int i, int j) {
-		return (i - 1) * width + j - 1;
-	}
+    protected int index(int i, int j) {
+        return (i - 1) * width + j - 1;
+    }
 
-	protected int[] getNeighbors(int i, int j) {
-		int[] positions = new int[4];
-		// up
-		int count = 0;
-		if (valid(i - 1, j)) {
-			positions[count] = index(i - 1, j);
-			count++;
-		}
-		// right
-		if (valid(i, j + 1)) {
-			positions[count] = index(i, j + 1);
-			count++;
-		}
-		// down
-		if (valid(i + 1, j)) {
-			positions[count] = index(i + 1, j);
-			count++;
-		}
-		// left
-		if (valid(i, j - 1)) {
-			positions[count] = index(i, j - 1);
-			count++;
-		}
-		int[] result = new int[count];
-		for (int ix = 0; ix < count; ix++) {
-			result[ix] = positions[ix];
-		}
-		return result;
-	}
+    protected int[] getNeighbors(int i, int j) {
+        int[] positions = new int[4];
+        // up
+        int count = 0;
+        if (valid(i - 1, j)) {
+            positions[count] = index(i - 1, j);
+            count++;
+        }
+        // right
+        if (valid(i, j + 1)) {
+            positions[count] = index(i, j + 1);
+            count++;
+        }
+        // down
+        if (valid(i + 1, j)) {
+            positions[count] = index(i + 1, j);
+            count++;
+        }
+        // left
+        if (valid(i, j - 1)) {
+            positions[count] = index(i, j - 1);
+            count++;
+        }
+        int[] result = new int[count];
+        for (int ix = 0; ix < count; ix++) {
+            result[ix] = positions[ix];
+        }
+        return result;
+    }
 
-	protected boolean valid(int i, int j) {
-		if (i > width || j > width || i < 1 || j < 1) {
-			return false;
-		} else {
-			return true;
-		}
-	}
+    protected boolean valid(int i, int j) {
+        if (i > width || j > width || i < 1 || j < 1) {
+            return false;
+        }
+        return true;
+    }
 
 }
--- src/main/java/PercolationStats.java
+++ src/main/java/PercolationStats.java
@@ -1,66 +1,54 @@
 public class PercolationStats {
 
-	private final double[] frac;
-	private int T;
+    private final double[] frac;
+    private int T;
 
-	public PercolationStats(int N, int T) {
-		this.T = T;
-		frac = new double[T];
-		if (N <= 0 || T <= 0) {
-			throw new IllegalArgumentException(
-					"N and T could not be less or equal than 0");
-		}
-		for (int t = 0; t < T; t++) {
-			Percolation percolation = new Percolation(N);
-			double count = 0;
-			while (!percolation.percolates()) {
-				int i = StdRandom.uniform(1, N+1);
-				int j= StdRandom.uniform(1, N+1);
-				if(!percolation.isOpen(i, j)){
-					percolation.open(i, j);
-					count++;
-				}
-			}
-			frac[t] = count / (N * N);
-		}
+    public PercolationStats(int N, int T) {
+        this.T = T;
+        frac = new double[T];
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException(
+                    "N and T could not be less or equal than 0");
+        }
+        for (int t = 0; t < T; t++) {
+            Percolation percolation = new Percolation(N);
+            double count = 0;
+            while (!percolation.percolates()) {
+                int i = StdRandom.uniform(1, N + 1);
+                int j = StdRandom.uniform(1, N + 1);
+                if (!percolation.isOpen(i, j)) {
+                    percolation.open(i, j);
+                    count++;
+                }
+            }
+            frac[t] = count / (N * N);
+        }
 
-	}
+    }
 
-	public double mean() {
-		return StdStats.mean(frac);
-	}
+    public double mean() {
+        return StdStats.mean(frac);
+    }
 
-	public double stddev() {
-		return StdStats.stddev(frac);
-	}
+    public double stddev() {
+        return StdStats.stddev(frac);
+    }
 
-	public double confidenceLo() {
-		return this.mean() - (1.96 * this.stddev() / Math.sqrt(T));
-	}
+    public double confidenceLo() {
+        return this.mean() - (1.96 * this.stddev() / Math.sqrt(T));
+    }
 
-	public double confidenceHi() {
-		return this.mean() + (1.96 * this.stddev() / Math.sqrt(T));
-	}
+    public double confidenceHi() {
+        return this.mean() + (1.96 * this.stddev() / Math.sqrt(T));
+    }
 
-	public static void main(String[] args) {
-		if (args.length != 2) {
-			System.err
-					.println("[usage:] java PercolationStats [integer] [integer]");
-			System.exit(1);
-		}
-		int N = -1;
-		int T = -1;
-		try {
-			N = Integer.valueOf(args[0]);
-			T = Integer.valueOf(args[1]);
-		} catch (NumberFormatException e) {
-			System.err.println("The arguments have to be integers");
-			System.exit(1);
-		}
-		PercolationStats stats = new PercolationStats(N, T);
-		System.out.println("mean\t\t\t\t= " + stats.mean());
-		System.out.println("stddev\t\t\t\t= " + stats.stddev());
-		System.out.println("95% confidence interval\t\t= "
-				+ stats.confidenceLo() + ", " + stats.confidenceHi());
-	}
+    public static void main(String[] args) {
+        int N = Integer.valueOf(args[0]);
+        int T = Integer.valueOf(args[1]);
+        PercolationStats stats = new PercolationStats(N, T);
+        System.out.println("mean\t\t\t\t= " + stats.mean());
+        System.out.println("stddev\t\t\t\t= " + stats.stddev());
+        System.out.println("95% confidence interval\t\t= "
+                + stats.confidenceLo() + ", " + stats.confidenceHi());
+    }
 }
